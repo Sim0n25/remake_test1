5526655cf96fa52a89d248ff15ef3cffd44392f2
Simon <simon.fransen@student.kdg.be>
2021-07-06 17:11:52 +0200

    klaar denk ik

--- toolscript.py
+++ toolscript.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+
+#een python toolscript om uw linux os te upgraden of maria db te installeren
+
 import os
 
 def main(): #functie
+
+    # VARIABLES
     mariaDB ="sudo apt install mariadb-server"#variabele is mariaDB en die leest dat als mariadb-server
+
+    # PRINTS
     print("upgrade = upgrade your linux")
     print("mariaDB = install mariadb")
 
@@ -14,16 +21,9 @@ def main(): #functie
     elif keuze == "mariaDB" : #else if, zelfde als if maar dan 2e keer. of 3e of 4e..
         os.system(mariaDB)
         main()
-
-
     else:
         print("foute keuze") #als mijn keuze upgrade is doet die dat. Als dat niet een vd ifs erboven is gaat die else doen
         main() #terug nr begin
-
-
-
-
-
 
 
 if __name__ == '__main__':#als ik dit script run, runt de main
